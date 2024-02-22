b7fdb1ba170a2365a85d536fe3d23a7d0802b870
ddragonyt2 <japwalker@hotmail.com>
2024-02-23 09:03:38 +1100

    more songs

--- import random.py
+++ import random.py
@@ -2,7 +2,8 @@ import random
 
 userInput = int(input("How many times?"))
 startingNumber = int(input("Starting number?"))
-
+minChar = int(input("How many characters MIN?"))
+maxChar = int(input("How many characters MAX?"))
 
 counter = startingNumber
 song = ""
@@ -11,7 +12,7 @@ alphabet = "abcdefghijklmnopqrstuvwxyz"
 counter = 1
 for x in range(userInput):
     song = ""
-    for x in range(random.randint(3,6)):
+    for x in range(random.randint(minChar,maxChar)):
         song += alphabet[random.randint(0,24)]
     name = str(counter+startingNumber)+'.txt'  # Name of text file coerced with +.txt
     try:
